415efe01aca498392929d51f48a527eec41bc94e
deepakgusain-9730 <deepak.gusain1310@gmail.com>
2025-01-17 21:37:57 +0530

    Added Controller Advice exception handler

--- src/main/java/com/learning/firstspringapi/controllers/ProductController.java
+++ src/main/java/com/learning/firstspringapi/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.learning.firstspringapi.controllers;
 import com.learning.firstspringapi.dto.FakeStoreProductDto;
 import com.learning.firstspringapi.models.Product;
 import com.learning.firstspringapi.services.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController // This controller going to host rest api
 @RequestMapping("products") // products endpoint will come to this class from handler mapping by Dispatcher ServerLet
 public class ProductController {
@@ -34,7 +36,13 @@ public class ProductController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
-        Product product1 = productService.updateProduct(id, product);
-        return new ResponseEntity<Product>(product1, HttpStatus.OK);
+        try {
+            Product product1 = productService.updateProduct(id, product);
+            return new ResponseEntity<Product>(product1, HttpStatus.OK);
+        }
+        catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
